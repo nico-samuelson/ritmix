8f4336a53c799820ed04ed3ec2bdaffc7ae017a9
nico-samuelson <nicos.t.65@gmail.com>
2025-03-03 14:30:46 +0700

    Initial Commit

---
+++ ritmix/ritmixApp.swift
@@ -0,0 +1,17 @@
+//
+//  ritmixApp.swift
+//  ritmix
+//
+//  Created by Nico Samuelson on 03/03/25.
+//
+
+import SwiftUI
+
+@main
+struct ritmixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
